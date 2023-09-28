7667ef43dee2c9a3a16f245c1910fdd82f448d12
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-28 18:11:56 +0900

    💄 ログインの有無に応じてサイドバーのUI/UXを変更

--- src/components/layout/SidebarItem.tsx
+++ src/components/layout/SidebarItem.tsx
@@ -1,21 +1,38 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useCallback } from 'react'
 import type { IconType } from 'react-icons'
+
+import useCurrentUser from '@/hooks/useCurrentUser'
+import useLoginModal from '@/hooks/useLoginModal'
 
 type SidebarItemProps = {
   href?: string
   label: string
   icon: React.ReactElement
+  auth?: boolean
   onClick?: () => void
 }
 
-const SidebarItem: React.FC<SidebarItemProps> = ({ href, label, icon, onClick }) => {
+const SidebarItem: React.FC<SidebarItemProps> = ({ href, label, icon, auth, onClick }) => {
+  const loginModal = useLoginModal()
   const router = useRouter()
+  const { data: currentUser } = useCurrentUser()
+
+  const handleClick = useCallback(() => {
+    if (onClick) return onClick()
+
+    if (auth && (!currentUser || currentUser.message)) {
+      console.log('OK')
+      loginModal.onOpen()
+    } else if (href) {
+      router.push(href)
+    }
+  }, [onClick, auth, currentUser, href, loginModal, router])
 
   return (
-    <div className="flex flex-row items-center" onClick={onClick}>
+    <div className="flex flex-row items-center" onClick={handleClick}>
       <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
         {React.cloneElement(icon, { size: 28, color: 'white' })}
       </div>
