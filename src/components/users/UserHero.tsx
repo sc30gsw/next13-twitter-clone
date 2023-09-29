331db9ab8cc8326a9ca9fc8c511b4e5c48ea60de
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-29 16:42:46 +0900

    ✨ 画像アップロード機能の実装

--- src/components/users/UserHero.tsx
+++ src/components/users/UserHero.tsx
@@ -1,20 +1,22 @@
+'use client'
+
 import Image from 'next/image'
 import React from 'react'
 
 import Avatar from '@/components/Avatar'
-import useFetchUser from '@/hooks/useFetchUser'
+import useUser from '@/hooks/useUser'
 
 type UserHeroProps = {
   userId: string
 }
 
-const UserHero: React.FC<UserHeroProps> = async ({ userId }) => {
-  const fetchedUser = await useFetchUser(userId)
+const UserHero: React.FC<UserHeroProps> = ({ userId }) => {
+  const { data: fetchedUser } = useUser(userId)
 
   return (
     <div>
       <div className="bg-neutral-700 h-44 relative">
-        {fetchedUser.user.coverImage && (
+        {fetchedUser?.user.coverImage && (
           <Image src={fetchedUser.user.coverImage} fill alt="Cover Image" className="object-cover" />
         )}
         <div className="absolute -bottom-16 left-4">
