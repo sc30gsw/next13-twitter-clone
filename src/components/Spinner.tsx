88aac6f6585ac95ba7da4985d83940645f8fd5ce
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-30 21:57:40 +0900

    💄 mt-2を追加

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -6,7 +6,7 @@ type SpinnerProps = {
 
 const Spinner: React.FC<SpinnerProps> = ({ color = 'border-blue-500' }) => {
   return (
-    <div className="flex justify-center items-center h-full">
+    <div className="flex justify-center items-center h-full mt-2">
       <div className={`h-10 w-10 animate-spin rounded-full border-4 ${color} border-t-transparent`}></div>
     </div>
   )
